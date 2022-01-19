563871f462c0779f9ed2d8b81c97b14ba7322207
Amitkumar Mishra <mr.mash@gmail.com>
2022-01-19 15:21:18 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# Schedule Library imported
+import schedule
+import time
+import mysql.connector
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Content
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host=os.environ.get("host"),
+    user=os.environ.get("user"),
+    password=os.environ.get("password"),
+    database=os.environ.get("database")
+)
+
+mycursor = mydb.cursor()
+
+
+def schedule_task():
+    mycursor.execute("SELECT * from Categories")
+    categories = mycursor.fetchall()
+    low_inventory = []
+    for x in categories:
+        check_inventory(x[0], x[1], low_inventory)
+    send_mail(low_inventory)
+
+
+def send_mail(low_inventory):
+    report = "<br>".join(low_inventory)
+    message = Mail(
+        from_email=os.environ.get("from_email"),
+        to_emails=os.environ.get("to_email"))
+    message.subject = "ALERT: Low stocks report"
+    message.content = Content("text/html", report)
+    try:
+        sg = SendGridAPIClient(os.environ.get('sendgrid_api_key'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
+
+
+def check_inventory(category_id, category_name, low_inventory):
+    mycursor.execute("SELECT count(*) from Products Where category_id = % s" % category_id)
+    result = mycursor.fetchall()
+    if result[0][0] < 35:
+        low_inventory.append("Stock count of {} : {}".format(category_name, result[0][0]))
+
+
+schedule.every(6).hours.do(schedule_task)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
